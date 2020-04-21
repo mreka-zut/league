d5ca859cc2c5e3566f3bb94f82d6f64cc79567a6
mreka-zut <62557954+mreka-zut@users.noreply.github.com>
2020-04-21 18:29:49 +0200

    update riot api key

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class MyAppState extends State<MyApp> {
   Future<Map<String, dynamic>> mapSnapshot;
   Future<String> fetchRoster() async {
     final response = await http.get(
-        'https://eun1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-5dcf5863-1239-4eb1-b276-3917e226e81e');
+        'https://eun1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-d892e894-1a1b-41e9-aca8-f38f849355d0');
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
       // then parse the JSON.
